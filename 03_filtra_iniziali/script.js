1b0d814c717854f4c7f5fbbf9dcde7daf56b0da9
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-09-03 16:07:24 +0200

    Completa esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function searchLetter (arr, letter){
+    const result = []
+    for (let i = 0;i < arr.length; i++){
+        if (arr[i][0] === letter){
+            result.push(arr[i]);
+        }
+    }
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(searchLetter(names, "A"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
